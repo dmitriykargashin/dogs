2d89160519c41a5a69172d4bab077f56f3049b80
dmitriikargashin <dmitriy.kargashin@gmail.com>
2020-01-15 09:05:48 -0500

    first commit

---
+++ app/src/main/java/com/cardamon/dogs/view/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.cardamon.dogs.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.cardamon.dogs.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
