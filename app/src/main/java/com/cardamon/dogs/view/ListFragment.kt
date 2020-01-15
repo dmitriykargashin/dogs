faf9afa0a0536134ed7731a5d9929188e67640be
dmitriikargashin <dmitriy.kargashin@gmail.com>
2020-01-15 09:39:28 -0500

    + Fragments Navigation implemented

--- app/src/main/java/com/cardamon/dogs/view/ListFragment.kt
+++ app/src/main/java/com/cardamon/dogs/view/ListFragment.kt
@@ -6,8 +6,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 
 import com.cardamon.dogs.R
+import kotlinx.android.synthetic.main.fragment_list.*
 
 /**
  * A simple [Fragment] subclass.
@@ -22,5 +24,13 @@ class ListFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_list, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        buttonDetails.setOnClickListener {
 
+            val action= ListFragmentDirections.actionDetailFragment()
+            Navigation.findNavController(it).navigate(action)
+        }
+
+    }
 }
